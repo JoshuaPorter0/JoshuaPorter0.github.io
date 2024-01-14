a3721ec84218b8c02e040f953c6473d82df4c211
Joshua Porter <porterjosh76@gmail.com>
2024-01-13 22:44:32 -0800

    Added Pong

--- _posts/2024-01-13-MondrianArtFun.md
+++ _posts/2024-01-13-MondrianArtFun.md
@@ -6,19 +6,27 @@ categories: Projects
 ---
 
 
-
-<link rel="stylesheet" href="/assets/css/Mondrian/style.css">
-<script src="/assets/js/Mondrian/script.js"></script>
-
+<!-- Mondrian art HTML ge -->
+<link rel="stylesheet" href="/assets/css/Mondrian/Mondrian.css">
+<script src="/assets/js/Mondrian/Mondrian.js"></script>
+<link rel="stylesheet" href="/assets/css/Mondrian/Pong.css">
+<script src="/assets/js/Mondrian/Pong.js"></script>
 <h1>Mondrian Art Creator</h1>
 <p id="instructions">Click any box to make your own Mondrian art - or click generate to make a new template.</p>
 <button id="generate-button">Generate</button>
 <div id="art-container"></div>
 <!-- Grid cells will be generated here by JavaScript -->
 
+--------------------------------------------------
+--------------------------------------------------
+--------------------------------------------------
+And we'll put pong here too because why not. (move with WS and arrow keys)
 
+<canvas id="pongCanvas" width="800" height="400" tabindex="0"></canvas>
 
-
+--------------------------------------------------
+--------------------------------------------------
+--------------------------------------------------
 
 Josh Jotting:
 
@@ -27,6 +35,8 @@ Grimoire did most of the heavy lifting here but it definitely helped knowing fou
 I quite like the mondrian art style and I enjoyed the opportunity to try to tackle a problem by utilizing AI, though it was nowhere near as difficult as the project of spray painting my car which I undertook in June of 2022. 
 
 Here are some photos of the Mondrian car, Wanda:
+
+
 ![Wanda](/assets/Wanda/DesertWanda.jpg)
 ![Wanda](/assets/Wanda/WandaHauler.jpg)
 ![Wanda](/assets/Wanda/WillametteWanda.jpg)
---
+++ assets/js/Mondrian/Pong.js
@@ -0,0 +1,108 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Your Pong game code here
+    const canvas = document.getElementById('pongCanvas');
+    canvas.addEventListener('keydown', function(event) {
+        // Your existing switch statement...
+        
+        // Prevent default action for arrow keys
+        if([38, 40, 87, 83].includes(event.keyCode)) {
+            event.preventDefault();
+        }
+    });
+    
+    const context = canvas.getContext('2d');
+
+    // Game elements: Ball, Paddles, Scores
+    let ball, paddle1, paddle2, score1, score2;
+
+    // Initialize Game Elements
+    function initGame() {
+        ball = { x: canvas.width / 2, y: canvas.height / 2, radius: 10, speedX: 5, speedY: 5 };
+        paddle1 = { x: 0, y: canvas.height / 2 - 40, width: 10, height: 80 };
+        paddle2 = { x: canvas.width - 10, y: canvas.height / 2 - 40, width: 10, height: 80 };
+        score1 = score2 = 0;
+        window.requestAnimationFrame(drawGame);
+    }
+
+    // Draw Game Elements
+    function drawGame() {
+        // Clear canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw Ball
+        context.fillStyle = 'white';
+        context.beginPath();
+        context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fill();
+
+        // Draw Paddles
+        context.fillStyle = 'white';
+        context.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+        context.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+
+        // Update Game State
+        updateGame();
+
+        // Request next frame
+        window.requestAnimationFrame(drawGame);
+    }
+
+    // Update Game State: Ball Movement, Collision, Scoring
+    function updateGame() {
+        // Update Ball Position
+        ball.x += ball.speedX;
+        ball.y += ball.speedY;
+
+        // Ball Collision with Top/Bottom Walls
+        if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+            ball.speedY = -ball.speedY;
+        }
+
+        // Ball Collision with Paddles
+        if (ball.x - ball.radius < paddle1.x + paddle1.width && ball.y > paddle1.y && ball.y < paddle1.y + paddle1.height) {
+            ball.speedX = -ball.speedX;
+        }
+        if (ball.x + ball.radius > paddle2.x && ball.y > paddle2.y && ball.y < paddle2.y + paddle2.height) {
+            ball.speedX = -ball.speedX;
+        }
+
+        // Scoring
+        if (ball.x + ball.radius < 0) {
+            score2++;
+            resetBall();
+        } else if (ball.x - ball.radius > canvas.width) {
+            score1++;
+            resetBall();
+        }
+    }
+
+    // Reset Ball to Center after Scoring
+    function resetBall() {
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+        ball.speedX = -ball.speedX;
+    }
+
+    // Paddle Movement Controls
+    document.addEventListener('keydown', function(event) {
+        switch(event.keyCode) {
+            case 87: // W key
+                paddle1.y = Math.max(paddle1.y - 10, 0);
+                break;
+            case 83: // S key
+                paddle1.y = Math.min(paddle1.y + 10, canvas.height - paddle1.height);
+                break;
+            case 38: // Up Arrow
+                paddle2.y = Math.max(paddle2.y - 10, 0);
+                break;
+            case 40: // Down Arrow
+                paddle2.y = Math.min(paddle2.y + 10, canvas.height - paddle2.height);
+                break;
+        }
+    });
+
+    // Start Game
+    initGame();
+
+});
